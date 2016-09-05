cb5890f649330b78f277811fb1f322847cf12ca5
Marcos Vanetta <marcosvanetta@gmail.com>
2016-09-05 02:04:02 -0500

    Refactoring memory

--- src/memory.js
+++ src/memory.js
@@ -1,8 +1,88 @@
+class Registers {
+  constructor() {
+    this.A = 0;
+    this.F = 0;
+    this.B = 0;
+    this.C = 0;
+    this.D = 0;
+    this.E = 0;
+    this.H = 0;
+    this.L = 0;
+    this.SP = 0;
+    this.PC = 0;
+
+    this.flagHelpers = {
+      Z: 0b10000000,
+      N: 0b01000000,
+      H: 0b00100000,
+      C: 0b00010000,
+    };
+
+    this.cFlagHelpers = {
+      Z: 0b01111111,
+      N: 0b10111111,
+      H: 0b11011111,
+      C: 0b11101111,
+    };
+  }
+
+  get AF() {
+    return ((this.A << 8) | this.F);
+  }
+
+  get BC() {
+    return ((this.B << 8) | this.C);
+  }
+
+  get DE() {
+    return ((this.D << 8) | this.E);
+  }
+
+  get HL() {
+    return ((this.H << 8) | this.L);
+  }
+
+  set AF(val) {
+    this.A = (val >> 8) & 0b0000000011111111;
+    this.F = (val & 0b0000000011111111);
+  }
+
+  set BC(val) {
+    this.B = (val >> 8) & 0b0000000011111111;
+    this.C = (val & 0b0000000011111111);
+  }
+
+  set DE(val) {
+    this.D = (val >> 8) & 0b0000000011111111;
+    this.E = (val & 0b0000000011111111);
+  }
+
+  set HL(val) {
+    this.H = (val >> 8) & 0b0000000011111111;
+    this.L = (val & 0b0000000011111111);
+  }
+
+  setFlag(flag, val=true) {
+    /*
+    0bSZ-H -PNC --> Living in register F
+    */
+    if (val) {
+      this.F = this.F | this.flagHelpers[flag];
+    } else {
+      this.F = this.F & this.cFlagHelpers[flag];
+    }
+
+    return this.F;
+  }
+};
+
 exports.Memory = class Memory {
   constructor(data) {
-    this.data = new Uint8Array(data);
-    this.resetRegisters();
-    this.resetFlags();
+    this.data = new Uint8Array(65536);
+    this.r = new Registers();
+    for (let index = 0; index < data.length; index++) {
+      this.data[index] = data[index];
+    }
   }
 
   getInstruction(offset=0) {
@@ -11,15 +91,11 @@ exports.Memory = class Memory {
       It updates the PC
     */
     let addr = this.r.PC + offset;
-    if (addr > this.data.length) {
-      throw `addr: ${addr} is overflowing the memory`;
-    }
-
     this.r.PC = addr;
     return this.data[addr];
   }
 
-  writeByte(addr, value) {
+  writeByteAt(addr, value) {
     this.data[addr] = value;
   }
 
@@ -29,10 +105,6 @@ exports.Memory = class Memory {
 
   getByteAt(addr=0) {
     /* returns 8bit integer */
-    if (addr > this.data.length) {
-      throw `addr: ${addr} is overflowing the memory`;
-    }
-
     if (addr == 0xFF0F) {
       console.log('byte read from interrupt flag (IF)');
     } else if (addr == 0xFF44) {
@@ -45,47 +117,5 @@ exports.Memory = class Memory {
   getWordAt(addr) {
     /* returns 16bit integer */
     return this.data[addr] | (this.data[addr + 1] << 8);
-  }
-
-  setFlag(flag, val=true) {
-    // TODO: test this!
-    if (val) {
-      this.r.F = this.r.F | this.f[flag];
-    } else {
-      this.r.F = this.r.F & this.f[flag];
-    }
-  }
-
-  resetRegisters() {
-    this.r = {
-      A: 0,
-      F: 0,
-      B: 0,
-      C: 0,
-      D: 0,
-      E: 0,
-      H: 0,
-      L: 0,
-      SP: 0,
-      PC: 0,
-    };
-    return this.r;
-  }
-
-  resetFlags() {
-    this.f = {
-      Z: 0b10000000,
-      N: 0b01000000,
-      H: 0b00100000,
-      C: 0b00010000,
-    };
-    this.cF = {
-      Z: 0b01111111,
-      N: 0b10111111,
-      H: 0b11011111,
-      C: 0b11101111,
-    };
-
-    return this.f, this.cF;
   }
 };
