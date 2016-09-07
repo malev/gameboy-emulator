865c2e69b267144924de84283a352e2db0cb0cf4
Marcos Vanetta <marcosvanetta@gmail.com>
2016-09-07 11:53:55 -0500

    Refactor CPU

--- src/cpu.js
+++ src/cpu.js
@@ -1,450 +1,105 @@
-const $ = require('jquery');
 const sprintf = require('sprintf-js').sprintf;
-
-const imp = {
-  CALL_nn: function (mem, nn) {
-    /*
-      Push address of next instruction onto stack and then jump to address n.
-    */
-    let addr = mem.r.PC + 3; // Address to push to stack
-
-    mem.r.SP = mem.r.SP - 1;
-    mem.writeByte(mem.r.SP, addr >> 8); // H part of addr
-
-    mem.r.SP = mem.r.SP - 1;
-    mem.writeByte(mem.r.SP, addr & 0x00FF); // L part of addr
-
-    mem.r.PC = parseInt(nn);
-  },
-
-  POP_nn: function (mem, nn) {
-    /*
-      Pop two bytes off stack into register pair nn.
-      Increment Stack Pointer (SP) twice.
-      nn = AF,BC,DE,HL
-    */
-    mem.r[nn[1]] = mem.getByteAt(mem.r.SP);
-    mem.r.SP = mem.r.SP + 1;
-    mem.r[nn[0]] = mem.getByteAt(mem.r.SP);
-    mem.r.SP = mem.r.SP + 1;
-  },
-
-  PUSH_REG: function (mem, reg) {
-    // Reg is expected to be 16bits
-    if (reg in mem.r) {
-      throw 'Not implemented yet';
-    } else {
-      mem.r.SP = mem.r.SP - 1;
-      mem.writeByte(mem.r.SP, mem.r[reg[0]]);
-
-      mem.r.SP = mem.r.SP - 1;
-      mem.writeByte(mem.r.SP, mem.r[reg[1]]);
-    }
-  },
-
-  JP_cc_n: function (mem, n, cc) { // 0x20, ...
-    /*
-    Jump to address n if following condition is true:
-      n = two byte immediate value. (LSByte first.)
-
-      cc = NZ, Jump if Z flag is reset.
-      cc = Z,  Jump if Z flag is set.
-      cc = NC, Jump if C flag is reset.
-      cc = C,  Jump if C flag is set.
-    */
-    throw 'Not implemented yet';
-  },
-
-  DEC_reg: function (mem, reg) { //0x05
-    /*
-      Decrement register
-      Flags affected:
-        Z - Set if result is zero.
-        N - Set.
-        H - Set if no borrow from bit 4.
-        C - Not affected.
-    */
-    if (mem.r[reg] == 1) {
-      mem.setFlag('Z', true);
-    } else {
-      mem.setFlag('Z', false);
-    }
-
-    if ((mem.r[reg] & 0b00001111) > 1) {
-      mem.setFlag('H', true);
-    } else {
-      mem.setFlag('H', false);
-    }
-
-    mem.setFlag('N', true);
-
-    mem.r[reg] = (mem.r[reg] - 1) & 0x00FF;
-  },
-
-  LD_REG_nn: function (mem, reg, nn) { // 0x21, ...
-    /*
-    Move nn (16bits) into register reg
-    */
-
-    if (reg in mem.r) {
-      mem.r[reg] = nn;
-    } else {
-      // It's a compose register (ex: DE)
-      let addr = (mem.r.D << 8) | mem.r.E;
-      mem.r[addr] = nn;
-    }
-  },
-
-  LD_REG_n: function (mem, reg, n) {
-    /*
-    Move n (8bits) into register reg
-    */
-    mem.r[reg] = n;
-  },
-
-  LD_A_DE: function (mem) { // 0x1A
-    /* Load A from address pointed to by DE */
-    let addr = (mem.r.D << 8) | mem.r.E;
-    mem.r.A = mem.getByteAt(addr);
-  },
-
-  LD_HL_A: function (mem) {
-    /* Copy A to address pointed by HL" ("LD (HL),A */
-    let addr = (mem.r.H << 8) | mem.r.L;
-    mem.writeByte(addr, mem.r.A);
-  },
-
-  LD_r1_r2: function (mem, r1, r2) { // 0x4F
-    mem.r.r1 = mem.r.r2;
-  },
-
-  LDH_a8_REG: function (mem, reg, n) { // 0xE0
-    /* Put A into memory address $FF00 + n. */
-    let addr = n + 0xFF00;
-    mem.writeByte(addr, mem.r.A);
-  },
-
-  XOR_REG_n: function (mem, reg) {
-    /*
-    Logical exclusive OR n with register A, result in A.
-    flags: Z
-    */
-    mem.r[reg] = mem.r[reg] ^ mem.r[reg];
-    mem.setFlag('F');
-  },
-
-  JRNZr8: function (mem, n) { // 0x20
-    /*
-    Jump to n if Z flag is reset.
-    */
-
-    // TODO: Refactor this into JP_cc_n
-
-    if (!(mem.r.F & 0b10000000)) {
-      let signed = (n + Math.pow(2, 7)) % Math.pow(2, 8) - Math.pow(2, 7);
-      let jumpTo = ((signed + mem.r.PC) & 0x00FF);
-      mem.r.PC = jumpTo;
-    }
-  },
-
-  LD_HL_m_A: function (mem) {
-    /*
-    Put A into memory address HL. Decrement HL.
-    */
-    mem.r.HL = mem.r.A;
-    mem.r.HL = mem.r.HL - 1;
-  },
-
-  XOR_A_n: function (mem) { // 0xAF
-    /*
-    Logical exclusive OR n with register A, result in A.
-    flags: Z
-    */
-    mem.r.A = mem.r.A ^ mem.r.A;
-    mem.setFlag('F');
-  },
-
-  LD_C_A: function (mem) { // 0xE2
-    /* Put A into address $FF00 + register C */
-    let addr = 0xFF00 + mem.r.C;
-    mem.writeByte(addr, mem.r.A);
-  },
-
-  ENABLE_CB: function (args) {},
-
-  // Prefix CB
-  RL_n: function (mem, n) { // 0x11
-    /* Rotate n left through Carry flag */
-
-    if (0b00010000 & mem.r.F == 0b00010000) {
-      mem.r[n] = (mem.r[n] << 1 | 0b00000001) & 0x00FF;
-    } else {
-      mem.r[n] = (mem.r[n] << 1 | 0b11111110) & 0x00FF;
-    }
-
-    mem.setFlag('C', (0b10000000 & mem.r[n]) == 0b10000000);
-    mem.setFlag('F', mem.r.A == 0);
-  },
-
-  BIT_7_H: function (mem) { // 0x7C
-    mem.setFlag('Z', !(0b10000000 & mem.r.H));
-  },
-};
-
-const opcodes = {
-  '0x05': {
-    original: 'DEC B',
-    fn: 'DEC_reg',
-    register: 'B',
-    flags: 'Z1H',
-    cycles: 4,
-    PC: 1,
-  },
-  '0x06': {
-    original: 'LD B,d8',
-    fn: 'LD_REG_n',
-    immediate8: true,
-    register: 'B',
-    flags: 'flagsZNHC',
-    cycles: 8,
-    PC: 2,
-  },
-  '0x0E': {
-    original: 'LD C,d8',
-    fn: 'LD_REG_n',
-    register: 'C',
-    cycles: 8,
-    PC: 2,
-  },
-  '0x11': {
-    original: 'LD DE,d16',
-    fn: 'LD_REG_nn',
-    register: 'DE',
-    immediate16: true,
-    cycles: 12,
-    PC: 3,
-  },
-  '0x17': {
-    original: 'RLA',
-    fn: 'RL_n',
-    register: 'A',
-    cycles: 4,
-    PC: 1,
-  },
-  '0x1A': {
-    original: 'LD A,(DE)',
-    fn: 'LD_A_DE',
-    cycles: 8,
-    PC: 1,
-  },
-  '0x20': {
-    original: 'JR NZ,r8',
-    fn: 'JRNZr8',
-    immediate8: true,
-    cycles: 8,
-    PC: 2,
-  },
-  '0x21': {
-    original: 'LD HL,d16',
-    fn: 'LD_REG_nn',
-    immediate16: true,
-    register: 'HL',
-    cycles: 12,
-    PC: 3,
-  },
-  '0x22': {
-
-  },
-  '0x31': {
-    original: 'LD SP,d16',
-    fn: 'LD_REG_nn',
-    immediate16: true,
-    register: 'SP',
-    cycles: 12,
-    PC: 3,
-  },
-  '0x32': {
-    original: 'LD (HL-),A',
-    fn: 'LD_HL_m_A',
-    cycles: 8,
-    PC: 1,
-  },
-  '0x3E': {
-    original: 'LD A,d8',
-    fn: 'LD_REG_n',
-    register: 'A',
-    cycles: 8,
-    PC: 2,
-  },
-  '0x4F': {
-    original: 'LD C,A',
-    fn: 'LD_r1_r2',
-    registers: ['C', 'A'],
-    cycles: 4,
-    PC: 1,
-  },
-  '0x77': {
-    original: 'LD (HL),A',
-    fn: 'LD_HL_A',
-    register: '',
-    cycles: 8,
-    PC: 1,
-  },
-  '0xAF': {
-    original: 'XOR A',
-    fn: 'XOR_A_n',
-    cycles: 4,
-    register: 'A',
-    flags: ['Z'],
-    PC: 1,
-  },
-  '0xC5': {
-    original: 'PUSH BC',
-    fn: 'PUSH_REG',
-    register: 'BC',
-    cycles: 16,
-    PC: 1,
-  },
-  '0xC1': {
-    original: 'POP BC',
-    fn: 'POP_nn',
-    registers: ['B', 'C'],
-    cycles: 12,
-    PC: 1,
-  },
-  '0xCB': {
-    original: 'PREFIX CB',
-    fn: 'ENABLE_CB',
-    immediate8: true,
-    cycles: 1,
-    PC: 1,  // It's actually 4 but I'll let the second function the update that
-  },
-  '0xCD': {
-    original: 'CALL a16',
-    fn: 'CALL_nn',
-    immediate16: true,
-    cycles: 12,
-    PC: 0,
-  },
-  '0xE0': {
-    original: 'LDH (a8),A',
-    fn: 'LDH_a8_REG',
-    immediate8: true,
-    register: 'A',
-    cycles: 12,
-    PC: 2,
-  },
-  '0xE2': {
-    original: 'LD (C),A',
-    fn: 'LD_C_A',
-    cycles: 8,
-    PC: 2,
-  },
-};
-
-const cbOpcodes = {  // Prefix CB
-  '0x11': {
-    original: 'RL C',
-    fn: 'RL_n',
-    register: 'C',
-    cycles: 8,
-    PC: 1,
-  },
-  '0x7C': {
-    original: 'BIT 7,H',
-    fn: 'BIT_7_H',
-    flags: ['Z'],
-    cycles: 8,
-    PC: 1,
-  },
-};
-
-exports.run = function (mem, instruction) {
-
-  let instr = `0x${sprintf('%02X', rom[pc])}`;
-
-  if (!(instr in opcodes)) {
-    throw `Instruction ${instr} in ${pc} not implemented.`;
-  }
-
-  let cmd = opcodes[instr];
-
-  return pc + cmd.PC;
-};
+const opcodes = require('opcodes').opcodes;
+const cbOpcodes = require('opcodes').cbOpcodes;
+const imp = require('imp').imp;
 
 exports.CPU = class CPU {
   constructor(mem) {
     this.mem = mem;
     this.imp = imp;
+    this.opcodes = opcodes;
+    this.cbOpcodes = cbOpcodes;
   }
 
   getInstruction () {
     let addr = this.mem.r.PC;
     let opcodedInstruction = `0x${sprintf('%02X', this.mem.getByteAt(addr))}`;
-    if (!(opcodedInstruction in opcodes)) {
-      throw `Instruction ${opcodedInstruction} in ${this.mem.pc} not implemented.`;
+    if (!(opcodedInstruction in this.opcodes)) {
+      throw `Instruction ${opcodedInstruction} in 0x${sprintf('%02X', addr)} not implemented.`;
     }
 
-    return opcodes[opcodedInstruction];
+    let instruction = this.opcodes[opcodedInstruction];
+    instruction.opcode = opcodedInstruction;
+    return instruction;
   }
 
   getCBInstruction () {
     this.mem.r.PC += 1;
     let addr = this.mem.r.PC;
     let opcodedCBInstruction = `0x${sprintf('%02X', this.mem.getByteAt(addr))}`;
-    if (!(opcodedCBInstruction in cbOpcodes)) {
+    if (!(opcodedCBInstruction in this.cbOpcodes)) {
       throw `Instruction ${opcodedCBInstruction} in ${this.mem.pc} not implemented [CB].`;
     }
 
-    return cbOpcodes[opcodedCBInstruction];
+    let instruction = this.cbOpcodes[opcodedCBInstruction];
+    instruction.opcode = opcodedCBInstruction;
+    return instruction;
   }
 
-  display (command, args) {
-
-    let addr = `0x${sprintf('%02X', this.mem.r.PC)}`;
-    let output = `${addr}: ${command} | 0x${sprintf('%02X', this.mem.getCurrentByte())}`;
-    if (typeof args === 'undefined') {
-      output += '\n';
-    } else {
-      output += `with ${args}\n`;
-    }
-
-    $('#steps').prepend(output);
-  }
-
-  run() {
-    /*
-      Looks for instruction in memory located at PC
-      Runs the instruction
-      Updates registers
-      Updates flags
-      Updates PC
-    */
-    let cmd = this.getInstruction();
+  getInstructionWithArguments () {
+    let instruction = this.getInstruction();
     let args = [];
 
-    if (cmd.fn == 'ENABLE_CB') {
-      cmd = this.getCBInstruction();
+    if (instruction.fn == 'ENABLE_CB') {
+      instruction = this.getCBInstruction();
     }
 
-    if ('register' in cmd) {
-      args.push(cmd.register);
-    } else if ('registers' in cmd) {
-      args.concat(cmd.registers);
+    if ('register' in instruction) {
+      args.push(instruction.register);
     }
 
-    if ('immediate16' in cmd) {
+    if ('registers' in instruction) {
+      args = args.concat(instruction.registers);
+    }
+
+    if ('other' in instruction) {
+      args.push(instruction.other);
+    }
+
+    if ('immediate16' in instruction) {
       // This will be argument nn
       args.push(this.mem.getWordAt(this.mem.r.PC + 1));
-    } else if ('immediate8' in cmd) {
+    } else if ('immediate8' in instruction) {
       // This will be argument n
       args.push(this.mem.getByteAt(this.mem.r.PC + 1));
     }
 
-    this.display(cmd.original);
-    this.imp[cmd.fn](this.mem, args);
+    return [instruction, args];
+  }
 
-    this.mem.r.PC += cmd.PC;
-    return this.mem.r.PC;
+  parseNext () {
+    if ((0x93 < this.mem.r.PC) & (this.mem.r.PC < 0xE0)) {
+      this.mem.r.PC = 0xE0;
+    }
+
+    let temp = this.getInstructionWithArguments();
+    let instruction = temp[0];
+    instruction.address = this.mem.r.PC;
+    // let args = temp[1];
+
+    if (instruction.PC === 0) {
+      this.mem.r.PC += 1;
+    } else {
+      this.mem.r.PC += instruction.PC;
+    }
+
+    return instruction;
+  }
+
+  exec () {
+    let temp = this.getInstructionWithArguments();
+    let instruction = temp[0];
+    let args = temp[1];
+
+    // Arguments order: [register, other, immediate]
+    if (typeof this.imp[instruction.fn] === 'function') {
+      this.imp[instruction.fn](this.mem, ...args);
+    } else {
+      console.log(`${instruction.original} ${instruction.fn} ${args}`);
+      throw `${instruction.fn} not defined.`;
+    }
+
+    this.mem.r.PC += instruction.PC;
+    return instruction;
   }
 };
--- src/imp.js
+++ src/imp.js
@@ -1,4 +1,4 @@
-exports.imp = {
+const imp = {
   helpers: {
     HELLO_CPU: function () {
       return 'hello cpu';
@@ -148,17 +148,46 @@ exports.imp = {
     throw 'AND_n not implemented yet';
   },
 
-  BIT_b_n: function (mem, b, n) {
+  BIT_b_n: function (mem, r, b) {
     /*
       BIT b,r - Test bit b in register r.
       b = 0-7, r = A,B,C,D,E,H,L,(HL)
+
       Flags affected:
-      Z - Set if bit b of register r is 0.
-      N - Reset.
-      H - Set.
-      C - Not affected.
+        Z - Set if bit b of register r is 0.
+        N - Reset.
+        H - Set.
+        C - Not affected.
     */
-    throw 'BIT_b_n not implemented yet';
+
+    mem.r.resetFlags();
+    mem.r.setFlag('H');
+
+    if (b === 7) {
+      mem.r.setFlag('Z', !(0b10000000 & mem.r[r]));
+      return;
+    } else if (b === 6) {
+      mem.r.setFlag('Z', !(0b01000000 & mem.r[r]));
+      return;
+    } else if (b === 5) {
+      mem.r.setFlag('Z', !(0b00100000 & mem.r[r]));
+      return;
+    } else if (b === 4) {
+      mem.r.setFlag('Z', !(0b00010000 & mem.r[r]));
+      return;
+    } else if (b === 3) {
+      mem.r.setFlag('Z', !(0b00001000 & mem.r[r]));
+      return;
+    } else if (b === 2) {
+      mem.r.setFlag('Z', !(0b00000100 & mem.r[r]));
+      return;
+    } else if (b === 1) {
+      mem.r.setFlag('Z', !(0b00000010 & mem.r[r]));
+      return;
+    } else if (b === 0) {
+      mem.r.setFlag('Z', !(0b00000001 & mem.r[r]));
+      return;
+    }
   },
 
   CALL_nn: function (mem, nn) {
@@ -169,7 +198,15 @@ exports.imp = {
       Flags affected:
       None
     */
-    throw 'CALL_n not implemented yet';
+    let addr = mem.r.PC + 3; // Address to push to stack
+
+    mem.r.SP = mem.r.SP - 1;
+    mem.writeByteAt(mem.r.SP, addr >> 8); // H part of addr
+
+    mem.r.SP = mem.r.SP - 1;
+    mem.writeByteAt(mem.r.SP, addr & 0x00FF); // L part of addr
+
+    mem.r.PC = nn;
   },
 
   CALL_cc_nn: function (mem, cc, nn) {
@@ -213,7 +250,27 @@ exports.imp = {
         H - Set if no borrow from bit 4.
         C - Set for no borrow. (Set if A < n.)
     */
-    throw 'CP_n not implemented yet';
+
+    mem.r.resetFlags();
+    if (['A', 'B', 'C', 'D', 'E', 'H', 'L'].indexOf(n) >= 0) {
+      throw 'CP_n not implemented yet';
+    } else if (n === 'HL') {
+      throw 'CP_n not implemented yet';
+    } else {
+      if (mem.r.A === n) {
+        mem.r.setFlag('Z', true);
+      }
+
+      mem.r.setFlag('N', true);
+
+      if ((mem.r.A & 0b00001111) > (n & 0b00001111)) {
+        mem.r.setFlag('H', true);
+      }
+
+      if (mem.r.A > n) {
+        mem.r.setFlag('C', true);
+      }
+    }
   },
 
   CPL: function (mem) {
@@ -256,7 +313,28 @@ exports.imp = {
         H - Set if no borrow from bit 4.
         C - Not affected.
     */
-    throw 'DEC_n not implemented yet';
+    let original;
+    if (n === 'HL') {
+      original = mem.getByteAt(mem.r.HL);
+    } else {
+      original = mem.r[n];
+    }
+
+    mem.r.resetFlags();
+    if (original === 1) {
+      mem.r.setFlag('Z', true);
+    }
+
+    if ((original & 0b00001111) > 1) {
+      mem.r.setFlag('H', true);
+    }
+
+    let result = (original - 1) & 0x00FF;
+    if (n == 'HL') {
+      mem.writeByteAt(mem.r.HL, result);
+    } else {
+      mem.r[n] = result;
+    }
   },
 
   DEC_nn: function (mem, nn) {
@@ -303,10 +381,26 @@ exports.imp = {
         H - Set if carry from bit 3.
         C - Not affected.
     */
-    throw 'INC_n not implemented yet';
+
+    if (n === 'HL') {
+      throw 'INC_n not implemented for HL';
+    }
+
+    mem.r.resetFlags();
+    let result = (mem.r[n] + 1) & 0x00FF;
+
+    if (result === 0) {
+      mem.r.setFlag('Z', true);
+    }
+
+    if ((n & 0b00001111) === 0b00001111) {
+      mem.r.setFlag('H', true);
+    }
+
+    mem.r[n] = result;
   },
 
-  INC_nn: function (mem, n) {
+  INC_nn: function (mem, nn) {
     /*
       INC nn - Increment register nn.
       n = BC,DE,HL,SP
@@ -314,7 +408,396 @@ exports.imp = {
       Flags affected:
         None
     */
-    throw 'INC_nn not implemented yet';
+    mem.r[nn] = (mem.r[nn] + 1) & 0x00FFFF;
+  },
+
+  JP_n: function (mem, n) {
+    /*
+      JP n - Jump to address n.
+      n = two byte immediate value. (LSByte first)
+      Flags affected:
+        None
+    */
+    throw 'JP_n not implemented yet';
+  },
+
+  JP_cc_n: function (mem, cc, n) {
+    /*
+      JP cc,n - Jump to address n if following condition is true:
+      n = two byte immediate value. (LSByte first.)
+      cc = NZ, Jump if Z flag is reset.
+      cc = Z,  Jump if Z flag is set.
+      cc = NC, Jump if C flag is reset.
+      cc = C,  Jump if C flag is set.
+      Flags affected:
+        None
+    */
+
+    let jump = false;
+
+    if (cc === 'NZ' & !(mem.r.F & 0b10000000)) {
+      jump = true;
+    } else if (cc === 'Z' & (mem.r.F & 0b10000000)) {
+      debugger;
+      jump = true;
+    } else if (cc === 'C' & (mem.r.C & 0b00010000)) {
+      jump = true;
+    } else if (cc === 'NC' & !(mem.r.C & 0b00010000)) {
+      jump = true;
+    }
+
+    if (jump) {
+      mem.r.PC = ((n + mem.r.PC) & 0x00FF);
+    }
+  },
+
+  LD_A_n: function (mem, n) {
+    /*
+      LD A,n - Put value n into A.
+      n = A,B,C,D,E,H,L,(BC),(DE),(HL),(nnnn),#
+    */
+    if (['BC', 'DE', 'HL'].indexOf(n) >= 0) {
+      mem.r.A = mem.getByteAt(mem.r[n]);
+    } else if (['A', 'B', 'C', 'D', 'E', 'H', 'L'].indexOf(n) >= 0) {
+      mem.r.A = mem.r[n];
+    } else {
+      mem.r.A = n;
+    }
+  },
+
+  LD_n_A: function (mem, n) {
+    /*
+      LD n,A - Put value A into n.
+      n = A,B,C,D,E,H,L,(BC,(DE),(HL),(nnnn)
+      Flags affected: None
+    */
+    if (['BC', 'DE', 'HL'].indexOf(n) >= 0) {
+      mem.writeByteAt(mem.r[n], mem.r.A);
+    } else if (['A', 'B', 'C', 'D', 'E', 'H', 'L'].indexOf(n) >= 0) {
+      mem.r[n] = mem.r.A;
+    } else {
+      mem.writeByteAt(n, mem.r.A);
+    }
+  },
+
+  LD_A_C: function (mem) {
+    /*
+      LD A,[C] - Put value at address $FF00 + register C into A.
+    */
+    throw 'LD_A_C not implemented yet';
+  },
+
+  LD_A_HLp: function (mem) {
+    /*
+      LD A,[HL+] - Same as LD A,[HLI].
+    */
+    throw 'LD_A_HLp not implemented yet';
+  },
+
+  LD_A_HLn: function (mem) {
+    /*
+      LD A,[HLI] - Put value at address HL into A. Increment HL.
+    */
+    throw 'LD_A_HLn not implemented yet';
+  },
+
+  LD_A_HLI: function (mem) {
+    /*
+      LD A,[HLI] - Put value at address HL into A. Increment HL.
+      Flags affected: None
+    */
+    throw 'LD_A_HLI not implemented yet';
+  },
+
+  LD_A_HLD: function (mem) {
+    /*
+      LD A,[HLD] - Put value at address HL into A. Decrement HL.
+      Flags affected: None
+    */
+    throw 'LD_A_HLD not implemented yet';
+  },
+
+  LD_C_A: function (mem) {
+    /*
+      LD [C],A - Put A into address $FF00 + register C.
+    */
+    mem.writeByteAt(mem.r.C + 0xFF00, mem.r.A);
+  },
+
+  LD_HLp_A: function (mem) {
+    /*
+      LD [HL+],A - Same as LD [HLI],A.
+    */
+    mem.writeByteAt(mem.r.HL, mem.r.A);
+    mem.r.HL = (mem.r.HL + 1) & 0x00FFFF;
+  },
+
+  LD_HLn_A: function (mem) {
+    /*
+      LD [HL-],A - Same as LD [HLD],A.
+    */
+    mem.writeByteAt(mem.r.HL, mem.r.A);
+    mem.r.HL = mem.r.HL - 1;
+  },
+
+  LD_HLI_A: function (mem) {
+    /*
+      LD [HLI],A - Put A into memory address HL. Increment HL.
+    */
+    throw 'LD_HLI_A not implemented yet';
+  },
+
+  LD_HLD_A: function (mem) {
+    /*
+      LD [HLD],A - Put A into memory address HL. Decrement HL.
+    */
+    throw 'LD_HLD_A not implemented yet';
+  },
+
+  LD_r1_r2: function (mem, r1, r2) {
+    /*
+      LD r1,r2 - Put value r2 into r1.
+    */
+    if (typeof r2 === 'string') {
+      mem.r[r1] = mem.r[r2];
+    } else {
+      mem.r[r1] = r2;
+    }
+  },
+
+  LD_n_nn: function (mem, n, nn) {
+    /*
+      LD n,nn - Put value nn into n.
+      n = BC,DE,HL,SP
+      nn = 16 bit immediate value
+      Flags affected: None
+    */
+    mem.r[n] = nn;
+  },
+
+  LD_HL_SP_n: function (mem, n) {
+    /*
+      LD HL,[SP+n] - Put SP + n into HL.
+
+      n = one byte signed immediate value
+
+      Flags affected:
+
+      Z - Reset.
+      N - Reset.
+      H - Set or reset according to operation.
+      C - Set or reset according to operation.
+    */
+    throw 'LD_HL_SP_n not implemented yet';
+  },
+
+  LD_SP_HL: function (mem) {
+    /*
+      LD SP,HL - Put HL into Stack Pointer (SP).
+      Flags affected: None
+    */
+    throw 'GENERIC_n not implemented yet';
+  },
+
+  LD_n_SP: function (mem, n) {
+    /*
+      LD [n],SP - Put Stack Pointer (SP) at address n.
+      n = two byte immediate address
+      Flags affected: None
+    */
+    throw 'LD_n_SP not implemented yet';
+  },
+
+  LDD_A_HL: function (mem, n) {
+    /*
+      LDD A,[HL] - Same as LD A,[HLD].
+    */
+    throw 'LDD_A_HL not implemented yet';
+  },
+
+  LDD_HL_A: function (mem, n) {
+    /*
+      LDD [HL],A - Same as LD [HLD],A.
+    */
+    throw 'LDD_HL_A not implemented yet';
+  },
+
+  LDH_n_A: function (mem, n) {
+    /*
+      LDH [n],A - Put A into memory address $FF00 + n.
+      n = one byte immediate value
+      Flags affected: None
+    */
+    mem.writeByteAt(0xFF00 + n, mem.r.A);
+  },
+
+  LDH_A_n: function (mem, n) {
+    /*
+      LDH A,[n] - Put memory address $FF00 + n into A.
+      n = one byte immediate value
+      Flags affected: None
+    */
+    debugger;
+    mem.r.A = mem.getByteAt((0xFF00 + n) & 0xFFFF);
+  },
+
+  LDHL_SP_n: function (mem, n) {
+    /*
+      LDHL SP,n - Same as LD HL,[SP+n]
+    */
+    throw 'LDHL_SP_n not implemented yet';
+  },
+
+  LDI_A_HL: function (mem, n) {
+    /*
+      LDI A,[HL] - Same as LD A,[HLI].
+    */
+    throw 'LDI_A_HL not implemented yet';
+  },
+
+  LDI_HL_A: function (mem, n) {
+    /*
+      LDI [HL],A - Same as LD [HLI],A.
+    */
+    throw 'LDI_HL_A not implemented yet';
+  },
+
+  NOP: function (mem) {
+    /*
+    NOP - No operation.
+    Flags affected: None
+    */
+    throw 'NOP not implemented yet';
+  },
+
+  OR_n: function (mem, n) {
+    /*
+      OR n - Logical OR n with register A, result in A.
+      n = A,B,C,D,E,H,L,(HL),#
+      Flags affected:
+        Z - Set if result is zero.
+        N - Reset.
+        H - Reset.
+        C - Reset.
+    */
+    throw 'OR_n not implemented yet';
+  },
+
+  POP_nn: function (mem, nn) {
+    /*
+      POP nn - Pop two bytes off stack into register pair nn.
+      Increment Stack Pointer (SP) twice.
+      nn = AF,BC,DE,HL
+      Flags affected: None
+    */
+    mem.r[nn[1]] = mem.getByteAt(mem.r.SP);
+    mem.r.SP = mem.r.SP + 1;
+    mem.r[nn[0]] = mem.getByteAt(mem.r.SP);
+    mem.r.SP = mem.r.SP + 1;
+  },
+
+  PUSH_nn: function (mem, nn) {
+    /*
+      PUSH nn - Push register pair nn onto stack.
+      Decrement Stack Pointer (SP) twice.
+      nn = AF,BC,DE,HL
+      Flags affected: None
+    */
+    mem.r.SP = mem.r.SP - 1;
+    mem.writeByteAt(mem.r.SP, mem.r[nn[0]]);
+
+    mem.r.SP = mem.r.SP - 1;
+    mem.writeByteAt(mem.r.SP, mem.r[nn[1]]);
+  },
+
+  RES_b_r: function (mem, n) {
+    /*
+      RES b,r - Reset bit b in register r.
+      b = 0-7, r = A,B,C,D,E,H,L,(HL)
+      Flags affected: None
+    */
+    throw 'RES_b_r not implemented yet';
+  },
+
+  RET: function (mem, n) {
+    /*
+      RET - Pop two bytes from stack & jump to that address.
+      Flags affected: None
+    */
+    let C = mem.getByteAt(mem.r.SP);
+    mem.r.SP +=  1;
+    let P = mem.getByteAt(mem.r.SP);
+    mem.r.SP +=  1;
+    mem.r.PC = (P << 8) | C;
+  },
+
+  RET_cc: function (mem, cc) {
+    /*
+      RET cc - Return if following condition is true:
+      cc = NZ, Return if Z flag is reset.
+      cc = Z,  Return if Z flag is set.
+      cc = NC, Return if C flag is reset.
+      cc = C,  Return if C flag is set.
+      Flags affected: None
+    */
+    throw 'RET_cc not implemented yet';
+  },
+
+  RETI: function (mem, n) {
+    /*
+      RETI - Pop two bytes from stack & jump to that
+      address then enable interrupts.
+      Flags affected: None
+    */
+    throw 'RETI not implemented yet';
+  },
+
+  RL_n: function (mem, n) {
+    /*
+      RL n - Rotate n left through Carry flag.
+      n = A,B,C,D,E,H,L,(HL)
+
+      Flags affected:
+        Z - Set if result is zero.
+        N - Reset.
+        H - Reset.
+        C - Contains old bit 7 data.
+    */
+    if (0b00010000 & mem.r.F == 0b00010000) {
+      mem.r[n] = (mem.r[n] << 1 | 0b00000001) & 0x00FF;
+    } else {
+      mem.r[n] = (mem.r[n] << 1 | 0b11111110) & 0x00FF;
+    }
+
+    mem.r.resetFlags();
+    mem.r.setFlag('C', (0b10000000 & mem.r[n]) == 0b10000000);
+    mem.r.setFlag('F', mem.r.A == 0);
+  },
+
+  SUB_n: function (mem, n) {
+    /*
+      SUB n - Subtract n from A.
+      n = A,B,C,D,E,H,L,(HL),#
+      Flags affected:
+        Z - Set if result is zero.
+        N - Set.
+        H - Set if no borrow from bit 4.
+        C - Set if no borrow.
+    */
+    throw 'SUB_n not implemented yet';
+  },
+
+  SWAP_n: function (mem, n) {
+    /*
+      SWAP n - Swap upper & lower bits of n.
+      n = A,B,C,D,E,H,L,(HL)
+      Flags affected:
+        Z - Set if result is zero.
+        N - Reset.
+        H - Reset.
+        C - Reset.
+    */
+    throw 'SWAP_n not implemented yet';
   },
 
   GENERIC_n: function (mem, n) {
@@ -334,6 +817,17 @@ exports.imp = {
       H - Reset.
       C - Reset.
     */
-    throw 'XOR_n not implemented yet';
+    mem.r.resetFlags();
+    mem.r.A = mem.r.A ^ mem.r[n];
+
+    if (mem.r.A === 0) {
+      mem.r.setFlag('Z', true);
+    }
+  },
+
+  NOT_IMPLEMENTED: function (mem, opcode) {
+    throw `${opcode} not implemented`;
   },
 };
+
+exports.imp = imp;
