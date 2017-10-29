f7276953b5ddeaabf70b936948bf23aee96aa11e
Marcos Vanetta <marcos_vanetta@condenast.com>
2017-10-29 13:29:20 -0500

    Replace jazmine with mocha

--- spec/impSpec.js
+++ spec/impSpec.js
@@ -1,48 +1,44 @@
+const expect = require('chai').expect;
 let imp = require('../src/imp').imp;
 let helpers = imp.helpers;
 const Memory = require('../src/memory').Memory;
 
 describe('helpers', function () {
-  it('says hello', function (done) {
-    expect(helpers.HELLO_CPU()).toBe('hello cpu');
-    done();
+  it('says hello', function () {
+    expect(helpers.HELLO_CPU()).to.equal('hello cpu');
   });
 
   describe('ADD_n1_n2', function () {
-    it('Add 1 + 1', function (done) {
+    it('Add 1 + 1', function () {
       let flags = [true, false, false, false];
       let result = helpers.ADD_n1_n2(0x0001, 0x0001, flags);
 
-      expect(result).toBe(0x0002);
-      expect(flags).toEqual([false, false, false, false]);
-      done();
+      expect(result).to.equal(0x0002);
+      expect(flags).to.be.deep.equal([false, false, false, false]);
     });
 
-    it('Add 0 + 0', function (done) {
+    it('Add 0 + 0', function () {
       let flags = [false, false, false, false];
       let result = helpers.ADD_n1_n2(0, 0, flags);
 
-      expect(result).toBe(0);
-      expect(flags).toEqual([true, false, false, false]);
-      done();
+      expect(result).to.equal(0);
+      expect(flags).to.be.deep.equal([true, false, false, false]);
     });
 
-    it('Add 0 + 0', function (done) {
+    it('Add 0 + 0', function () {
       let flags = [false, false, false, false];
       let result = helpers.ADD_n1_n2(0x0f, 0x01, flags);
 
-      expect(result).toBe(0x10);
-      expect(flags).toEqual([false, false, true, false]);
-      done();
+      expect(result).to.equal(0x10);
+      expect(flags).to.deep.equal([false, false, true, false]);
     });
 
-    it('Add 0 + 0', function (done) {
+    it('Add 0 + 0', function () {
       let flags = [false, false, false, false];
       let result = helpers.ADD_n1_n2(0xff, 1, flags);
 
-      expect(result).toBe(256); // TODO: verify this!
-      expect(flags).toEqual([false, false, true, true]);
-      done();
+      expect(result).to.equal(256); // TODO: verify this!
+      expect(flags).to.deep.equal([false, false, true, true]);
     });
   });
 
@@ -51,8 +47,8 @@ describe('helpers', function () {
       let flags = [false, false, false, false];
       let result = helpers.INC_n(1, flags);
 
-      expect(result).toBe(2);
-      expect(flags).toEqual([false, false, false, false]);
+      expect(result).to.equal(2);
+      expect(flags).to.deep.equal([false, false, false, false]);
       done();
     });
 
@@ -60,8 +56,8 @@ describe('helpers', function () {
       let flags = [false, false, false, false];
       let result = helpers.INC_n(0x0f, flags);
 
-      expect(result).toBe(0x10);
-      expect(flags).toEqual([false, false, true, false]);
+      expect(result).to.equal(0x10);
+      expect(flags).to.deep.equal([false, false, true, false]);
       done();
     });
   });
@@ -71,30 +67,27 @@ describe('helpers', function () {
       let flags = [false, false, false, false];
       let result = helpers.XOR_n1_n2(1, 2, flags);
 
-      expect(result).toBe(3);
-      expect(flags).toEqual([false, false, false, false]);
-      done();
+      expect(result).to.equal(3);
+      expect(flags).to.deep.equal([false, false, false, false]);
     });
 
-    it('XOR 4 ^ 4', function (done) {
+    it('XOR 4 ^ 4', function () {
       let flags = [false, false, false, false];
       let result = helpers.XOR_n1_n2(4, 4, flags);
 
-      expect(result).toBe(0);
-      expect(flags).toEqual([true, false, false, false]);
-      done();
+      expect(result).to.equal(0);
+      expect(flags).to.deep.equal([true, false, false, false]);
     });
   });
 
   describe('CP_n', function () {
-    it('A: 1, n: 1', function (done) {
+    it('A: 1, n: 1', function () {
       mem = new Memory([]);
       mem.r.A = 1;
 
       imp.CP_n(mem, 1);
 
-      expect(mem.r.F).toEqual(0b11000000);
-      done();
+      expect(mem.r.F).to.equal(0b11000000);
     });
 
     // it('A: 3, n: 2', function (done) {
